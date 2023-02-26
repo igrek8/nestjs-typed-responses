cc7b0a6314775fd2b146d7c2c108f43e48de9d83
Igor Korchagin <igor.korchagin8@gmail.com>
2023-02-26 21:51:51 +0100

    feat: export module options

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './api-type-metafield.decorator';
 export * from './collect-validation-errors';
 export * from './exceptions';
 export * from './typed-data-transfer-object';
+export * from './typed-response.module-options';
 export * from './typed-responses.module';
 export * from './validation-exception';
